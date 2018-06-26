2fdc700e93af65aa61f26902abb53b127a364033
Djack1010 <giachi.iada@gmail.com>
2018-06-26 15:52:38 +0200

    Add TestTemplateSup for supervised classification and TestTemplateUnsup for vectorization (unsupervised)

--- StandardParser.py
+++ StandardParser.py
@@ -3,7 +3,7 @@ import random
 
 import os
 
-def parse(data_path='./Example_Data/', shuffle=True):
+def parse(data_path='./Example_Data/', mode='unsup', shuffle=True):
 
     graphs = []
 
@@ -45,17 +45,24 @@ def parse(data_path='./Example_Data/', shuffle=True):
                 incoming_list = []
 
                 X.append(label_id)
-                Y.append(label_id)
+                if mode == 'unsup':
+                    Y.append(label_id)
+                
 
                 for i in range(0, len(adj_list)//2):
 
                     incoming_id = int(adj_list[i*2][1:])  # removes the (
-                    arc_label   = adj_list[i*2 + 1][:-1]  # removes the )
+                    arc_label   = int(adj_list[i*2 + 1][:-1])  # removes the )
                     incoming_list.append((incoming_id, arc_label))
 
                 adjacency_lists.append(incoming_list)
 
             # A graph is a tuple (X,Y,adj_lists,dim), where Y==X in unsup. tasks
+            if mode == 'sup':
+                if prefix[-1] == 0 :
+                    Y.append(0)
+                else:
+                    Y.append(1)
             graphs.append((np.array(X, dtype='int'), np.array(Y, dtype='int'), adjacency_lists, len(adjacency_lists)))
 
     if shuffle:
--- TestTemplateSup.py
+++ TestTemplateSup.py
@@ -13,7 +13,7 @@ DATA_PATH='./Example_Data/'
 name = 'TestTemplate'
 
 C = 40
-layer = 4
+layer = 2
 concatenate_fingerprints = True
 
 svmC = 100
@@ -21,12 +21,15 @@ gamma = 5
 unibigram = True
 
 threshold = 0.
-max_epochs = 15
+max_epochs = 3
 Lprec = np.array([1], dtype='int')
 
-graphs_train = parse(DATA_PATH)
+graphs_train = parse(DATA_PATH, 'sup')
 X_train, Y_train, adjacency_lists_train, sizes_train = unravel(graphs_train, one_target_per_graph=True)
 
+# OPEN A LOG FILE WHERE TO STORE RESULTS
+logging.basicConfig(
+    filename=name + '_' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '.log', level=logging.DEBUG, filemode='a')
 
 # PERFORM TRAINING over the entire training set
 runs = 1
@@ -45,7 +48,14 @@ for run in range(0, runs):
     unigram_train, allStates_train = compute_input_matrix(architecture, C, X_train, adjacency_lists_train, sizes_train,
                                                           concatenate=concatenate_fingerprints, return_all_states=True)
 
-    with open("fingerprints/" + name + '_' + str(run) + ' ' + str(C) + ' ' + str(layer) + ' ' + str(Lprec)
-                      + ' ' + str(concatenate_fingerprints), 'wb') as f:
 
-        pickle.dump([unigram_train, allStates_train, adjacency_lists_train, sizes_train, Y_train], f)
+    with open("fingerprints/" + name + '_' + str(run) + '_' + str(C) + '_' + str(layer) + '_' + str(Lprec)
+                      + '_' + str(concatenate_fingerprints), 'wb') as f:
+
+        # DEBUG: use a validation set equal to train. Just to make it work!
+        unigram_valid, allStates_valid, adjacency_lists_valid, sizes_valid, Y_valid = unigram_train, allStates_train, adjacency_lists_train, sizes_train, Y_train
+
+        pickle.dump([unigram_train, unigram_valid, allStates_train, allStates_valid, adjacency_lists_train, adjacency_lists_valid, sizes_train, sizes_valid, Y_train, Y_valid], f)
+
+
+    fingerprints_to_svm_accuracy(C, Lprec, layer, runs, svmC, gamma, concatenate_fingerprints, name)
